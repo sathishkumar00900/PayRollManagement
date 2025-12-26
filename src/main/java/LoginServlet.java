9774e04be1a73bab2cc0e9fea31d268fefe8b23b
sathishkumar00900 <sathishrsk0904@gmail.com>
2025-12-26 16:12:31 +0530

    initial Commit

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        String username= req.getParameter("username");
+        String password = req.getParameter("password");
+
+        DaoClass dao = new DaoClass();
+
+        if (dao.checkLogin(username, password)) {
+            res.sendRedirect("adminDashboard.html");
+        } else {
+            res.sendRedirect("login.html?error=true");
+        }
+    }
+}
+
+
